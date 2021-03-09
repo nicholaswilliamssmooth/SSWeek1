d95dc5ce47e19637fc7ee367379909e5faca9ffa
Connor <Connor@DESKTOP-SVR54RE>
2021-03-09 00:46:25 -0600

    The program has been completed

--- RandomGuess/src/MainClass.java
+++ RandomGuess/src/MainClass.java
@@ -1,18 +1,66 @@
+import java.util.Random;
+import java.util.Scanner;
 /**
  * 
  */
 
 /**
  * @author Connor
- *
+ * Program that generates a random number and has the user
+ * guess it
  */
 public class MainClass {
+	// constants
+	final static int LOWER_BOUND = 1;
+	final static int UPPER_BOUND = 100;
+	final static int APPROVED_RANGE = 10;
+	final static int MAX_CHANCES = 5;
 
 	/**
-	 * @param args
+	 * @param args is not used in this program
 	 */
 	public static void main(String[] args) {
-		System.out.println("biw");
+		// Declare and initialize variables
+		Random randInt = new Random();
+		Scanner userInput = new Scanner(System.in);
+		int userAnswer;
+		int generatedNum = randInt.nextInt(UPPER_BOUND-1) + LOWER_BOUND;
+		int chance = 0;
+		
+		System.out.println("A random number has been generated between"
+				+ " 1 and 100, \nCan you guess it?");
+		
+		/* 
+		 * We will either break out from getting the right 
+		 * answer, or exceeding our number of chances
+		*/
+		while (true) {
+			//System.out.println(generatedNum);
+			System.out.println("Guess the random number in the range of 1 and 100: ");
+			userAnswer = userInput.nextInt();
+			//System.out.println(userAnswer);
+			
+			// Success
+			if (Math.abs(userAnswer - generatedNum) <= APPROVED_RANGE) {
+				System.out.println("Congrats! You got within the allowed range of " 
+						+ APPROVED_RANGE + " and have won the game. \nYou guessed " + userAnswer +
+						" and the generated number was " + generatedNum + ".");
+				break;
+			}
+			else {
+				chance++;
+				System.out.println("Wrong answer!");
+				// Exceeding number of tries
+				if (chance == 5) {
+					System.out.println("Sorry, but you weren't able to guess correctly "
+							+ "after " + MAX_CHANCES + " attempts. The final answer was " + generatedNum + 
+							". \nYou have lost the game," + "but don't give up! Try again soon!");
+					break;
+				}
+				else { System.out.println("Try it again!"); }
+			}
+		}
+		
 
 	}
 
